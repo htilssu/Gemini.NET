83eb8820b3dbc5a9b39d90a7f9120ae19b1d4c67
PHAN Xuan Quang <20521008@ms.uit.edu.vn>
2025-04-20 15:26:47 +0700

    add example

--- Example APIs/Controllers/FileUploadController.cs
+++ Example APIs/Controllers/FileUploadController.cs
@@ -1,4 +1,7 @@
-﻿using GeminiDotNET;
+﻿using Gemini.NET.API_Models.API_Request;
+using GeminiDotNET;
+using GeminiDotNET.API_Models.Enums;
+using GeminiDotNET.Client_Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Example_APIs.Controllers
@@ -32,6 +35,33 @@ namespace Example_APIs.Controllers
             {
                 var name = await uploader.GetFileUriByDisplayNameAsync(displayName);
                 return Ok(name);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("AskFile")]
+        public async Task<IActionResult> AskFile(string apiKey, string filePath)
+        {
+            var uploader = new FileUploader(apiKey);
+
+            try
+            {
+                var fileUri = await uploader.UploadFileAsync(filePath);
+                var generatorWithApiKey = new Generator(apiKey);
+
+                var apiRequest = new ApiRequestBuilder()
+                    .WithPrompt("Summarize the context")
+                    .WithUploadedFile(fileUri, MimeType.ApplicationPdf)
+                    .WithDefaultGenerationConfig()
+                    .DisableAllSafetySettings()
+                    .Build();
+
+                var response = await generatorWithApiKey.GenerateContentAsync(apiRequest, "gemini-2.0-flash");
+
+                return Ok(response.Content);
             }
             catch (Exception ex)
             {
